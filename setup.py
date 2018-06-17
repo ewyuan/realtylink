e2ba152a738c5c069df97afe1f21cb468e0f23e6
Eric <eric.w.yuan@outlook.com>
2018-06-17 01:04:13 -0400

    Added requirements for package

--- setup.py
+++ setup.py
@@ -3,4 +3,10 @@ from setuptools import setup
 setup(name='realtylink',
       version='1.0',
       author='Eric Yuan',
-      packages=['realtylink'])
+      packages=['realtylink'],
+      install_requires=[
+            'schedule',
+            'pandas',
+            'requests',
+            'lxml']
+      )
